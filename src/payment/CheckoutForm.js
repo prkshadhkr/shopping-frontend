6b632dedbedf27e6f26c851efe3fe8f90b44206d
Prakash Adhikari <prakash.ad@gmail.com>
2020-09-06 21:37:22 -0400

    added form and stylesheets for form for stripe payment method

---
+++ src/payment/CheckoutForm.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js";
+import './Checkout.css';
+
+import { BASE_URL } from '../constants/generic';
+import { useSelector } from "react-redux";
+import { TOKEN_IN_STORAGE } from '../App';
+import { clear } from "redux-localstorage-simple";
+
+export default function CheckoutForm() {
+  const order = useSelector(st => st.orders.order);
+  const order_id = Object(order).id;
+  const _token = localStorage[TOKEN_IN_STORAGE];
+  console.log('+++++++ order_id +++++++++', order_id);
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+ 
+
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    window
+      .fetch(`${BASE_URL}/orders/payment`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({order_id, _token})
+      })
+      .then(res => {
+        clear();
+        return res.json();
+      })
+      .then(data => {
+        setClientSecret(data.clientSecret);
+      });
+  }, []);
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d"
+        }
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a"
+      }
+    }
+  };
+
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  const handleSubmit = async ev => {
+    ev.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: ev.target.name.value
+        }
+      }
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
+  };
+
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
+      <button className="payment-submit"
+        disabled={processing || disabled || succeeded}
+        id="submit"
+      >
+        <span id="button-text">
+          {processing ? (
+            <div className="spinner" id="spinner"></div>
+          ) : (
+            "Pay"
+          )}
+        </span>
+      </button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
+        </div>
+      )}
+      {/* Show a success message upon completion */}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded, see the result in your
+        <a
+          href={`https://dashboard.stripe.com/test/payments`}
+        >
+          {" "}
+          Stripe dashboard.
+        </a> Refresh the page to pay again.
+      </p>
+    </form>
+  );
+}
